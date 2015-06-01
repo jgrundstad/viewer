1122e014afa9e541c2a29a7b28e8b3825befb389
Jason Grundstad <jgrundstad@gmail.com>
2015-06-01 15:22:51 -0500

    new floatThead js for report headers

--- models.py
+++ models.py
@@ -37,6 +37,7 @@ class Sample(models.Model):
 
 class Bnid(models.Model):
     sample = models.ForeignKey(Sample)
+    #study = models.ForeignKey(Study)
     bnid = models.CharField(max_length=12, verbose_name='Bionimbus ID')
     description = models.CharField(max_length=256, verbose_name='Description',
                                    blank=True)
