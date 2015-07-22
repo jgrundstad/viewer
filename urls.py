1a3ef226f4cf6b6a8f557e6f0a70c6439ef4648f
Jason Grundstad <jgrundstad@gmail.com>
2015-07-22 14:06:57 -0500

    clean up

--- urls.py
+++ urls.py
@@ -3,7 +3,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 import views
-import update_views
 
 
 urlpatterns = patterns('',
@@ -16,6 +15,7 @@ urlpatterns = patterns('',
                            name='restricted'),
                        url(r'^permission/$', views.permission,
                            name='permission'),
+
                        # Bnid
                        url(r'^new_bionimbus_id/$',
                            views.new_bionimbus_id, name='new_bionimbus_id'),
@@ -23,6 +23,7 @@ urlpatterns = patterns('',
                            views.get_bnids_by_study, name='get_bnids_by_study'),
                        url(r'bnid_edit/(?P<bnid_id>\d+)/$',
                            views.edit_bionimbus_id, name='bnid_edit'),
+
                        # Sample
                        url(r'^new_sample/$', views.new_sample,
                            name='new_sample'),
@@ -30,10 +31,12 @@ urlpatterns = patterns('',
                            name='get_samples'),
                        url(r'sample_edit/(?P<sample_id>\d+)/$',
                            views.edit_sample, name='sample_edit'),
+
                        # Study
                        url(r'^new_study/$', views.new_study, name='new_study'),
                        url(r'study_edit/(?P<study_id>\d+)/$',
                            views.edit_study, name='study_edit'),
+
                        # Report
                        url(r'^upload_report/$', views.upload_report,
                            name='upload_report'),
@@ -44,4 +47,5 @@ urlpatterns = patterns('',
                            {'document_root': settings.MEDIA_ROOT}),
                        url(r'view_report/(?P<file_id>\d+)/$',
                            views.view_report, name='view_report'),
+
                        ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
