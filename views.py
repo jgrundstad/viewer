172da9b4d67f9e70fe0ebbb3cbbdcb31b4df31c6
Dominic Fitzgerald <dominicfitzgerald11@gmail.com>
2015-08-04 11:13:32 -0500

    Finished making structural changes to models

--- views.py
+++ views.py
@@ -92,11 +92,10 @@ def user_logout(request):
 Study model
 '''
 @user_passes_test(in_proj_user_group)
-def view_study(request):
-    studies = Study.objects.all()
-    context = {'studies': studies}
+def manage_study(request):
+    context = {'studies': Study.objects.all()}
     context.update(csrf(request))
-    return render_to_response('viewer/study/view_study.html', context,
+    return render_to_response('viewer/study/manage_study.html', context,
                               context_instance=RequestContext(request))
 
 @user_passes_test(in_proj_user_group)
@@ -105,7 +104,7 @@ def new_study(request):
         sform = StudyForm(request.POST, instance=Study())
         if sform.is_valid():
             sform.save()
-        return HttpResponseRedirect('/viewer/study/view_study/')
+        return HttpResponseRedirect('/viewer/study/')
     else:
         sform = StudyForm(instance=Study())
         context = {'study_form': sform}
@@ -120,7 +119,7 @@ def edit_study(request, study_id):
         updated_form = StudyForm(request.POST, instance=s)
         if updated_form.is_valid():
             updated_form.save()
-            return HttpResponseRedirect('/viewer/study/view_study/')
+            return HttpResponseRedirect('/viewer/study/')
     else:
         study_obj = Study.objects.get(pk=study_id)
         sform = StudyForm(instance=study_obj)
@@ -133,7 +132,7 @@ def edit_study(request, study_id):
 def delete_study(request, study_id):
     if request.method == 'POST':
         Study.objects.get(pk=study_id).delete()
-        return HttpResponseRedirect('/viewer/study/view_study/')
+        return HttpResponseRedirect('/viewer/study/')
     else:
         study_obj = Study.objects.get(pk=study_id)
         context = {'name': study_obj.name, 'pk': study_obj.pk}
@@ -142,60 +141,24 @@ def delete_study(request, study_id):
                                   context_instance=RequestContext(request))
 
 '''
-Bionimbus ID model
-'''
-@user_passes_test(in_proj_user_group)
-def new_bionimbus_id(request): #study_id=None, **kwargs):
-    #if study_id:
-        #print "Got study_id: {}".format(study_id)
-    if request.method == 'POST':
-        bform = BnidForm(request.POST, instance=Bnid())
-        print bform['sample']
-        print bform['bnid']
-        if bform.is_valid():
-            bform.save()
-        return HttpResponseRedirect('/viewer/new_bionimbus_id/')
-    else:
-        bform = BnidForm(instance=Bnid())
-        ss_form = StudySelectorForm()
-        context = {'bnid_form': bform, 'study_selector_form': ss_form}
-        bnids = Bnid.objects.all().order_by('-bnid')
-        context['bnids'] = bnids
-        context.update(csrf(request))
-        return render_to_response('viewer/bnid/new_bionimbus_id.html', context,
-                                  context_instance=RequestContext(request))
-
-@user_passes_test(in_proj_user_group)
-def edit_bionimbus_id(request, bnid_id):
-    if request.method == 'POST':
-        b = Sample.objects.get(pk=bnid_id)
-        updated_form = BnidForm(request.POST, instance=b)
-        if updated_form.is_valid():
-            updated_form.save()
-            return HttpResponseRedirect('/viewer/new_bionimbus_id/')
-    else:
-        bnid_obj = Bnid.objects.get(pk=bnid_id)
-        bform = BnidForm(instance=bnid_obj)
-        context = {'bnid_form': bform, 'bnid': bnid_obj.bnid, 'pk': bnid_obj.pk}
-        context.update(csrf(request))
-        return render_to_response('viewer/bnid/bnid_edit.html', context,
-                                  context_instance=RequestContext(request))
-
-'''
 Sample model
 '''
+@user_passes_test(in_proj_user_group)
+def manage_sample(request):
+    context = {'samples': Sample.objects.all()}
+    context.update(csrf(request))
+    return render(request, 'viewer/sample/manage_sample.html', context)
+
 @user_passes_test(in_proj_user_group)
 def new_sample(request):
     if request.method == 'POST':
         sform = SampleForm(request.POST, instance=Sample())
         if sform.is_valid():
             sform.save()
-        return HttpResponseRedirect('/viewer/new_sample/')
+        return HttpResponseRedirect('/viewer/sample/')
     else:
         sform = SampleForm(instance=Sample())
         context = {'sample_form': sform}
-        samples = Sample.objects.all()
-        context['samples'] = samples
         context.update(csrf(request))
         return render_to_response('viewer/sample/new_sample.html', context,
                                   context_instance=RequestContext(request))
@@ -207,18 +170,94 @@ def edit_sample(request, sample_id):
         updated_form = SampleForm(request.POST, instance=s)
         if updated_form.is_valid():
             updated_form.save()
-            return HttpResponseRedirect('/viewer/new_sample/')
+            return HttpResponseRedirect('/viewer/sample/')
     else:
         sample_obj = Sample.objects.get(pk=sample_id)
         sform = SampleForm(instance=sample_obj)
         context = {'sample_form': sform, 'name': sample_obj.name, 'pk': sample_obj.pk}
         context.update(csrf(request))
-        return render_to_response('viewer/sample/sample_edit.html', context,
+        return render_to_response('viewer/sample/edit_sample.html', context,
                                   context_instance=RequestContext(request))
+
+@user_passes_test(in_proj_user_group)
+def delete_sample(request, sample_id):
+    if request.method == 'POST':
+        Sample.objects.get(pk=sample_id).delete()
+        return HttpResponseRedirect('/viewer/sample')
+    else:
+        sample_obj = Sample.objects.get(pk=sample_id)
+        context = {'name': sample_obj.name, 'pk': sample_obj.pk}
+        context.update(csrf(request))
+        return render(request, 'viewer/sample/delete_sample.html', context)
+
+'''
+Bionimbus ID model
+'''
+@user_passes_test(in_proj_user_group)
+def manage_bnid(request):
+    context = {'bnids': Bnid.objects.all()}
+    context.update(csrf(request))
+    return render(request, 'viewer/bnid/manage_bnid.html', context)
+
+@user_passes_test(in_proj_user_group)
+def new_bnid(request): #study_id=None, **kwargs):
+    #if study_id:
+        #print "Got study_id: {}".format(study_id)
+    if request.method == 'POST':
+        bform = BnidForm(request.POST, instance=Bnid())
+        print bform['sample']
+        print bform['bnid']
+        if bform.is_valid():
+            bform.save()
+        return HttpResponseRedirect('/viewer/bnid/')
+    else:
+        bform = BnidForm(instance=Bnid())
+        ss_form = StudySelectorForm()
+        context = {'bnid_form': bform, 'study_selector_form': ss_form}
+        context.update(csrf(request))
+        return render_to_response('viewer/bnid/new_bnid.html', context,
+                                  context_instance=RequestContext(request))
+
+@user_passes_test(in_proj_user_group)
+def edit_bnid(request, bnid_id):
+    if request.method == 'POST':
+        b = Sample.objects.get(pk=bnid_id)
+        updated_form = BnidForm(request.POST, instance=b)
+        if updated_form.is_valid():
+            updated_form.save()
+            return HttpResponseRedirect('/viewer/bnid/')
+    else:
+        bnid_obj = Bnid.objects.get(pk=bnid_id)
+        bform = BnidForm(instance=bnid_obj)
+        ss_form = StudySelectorForm()
+        context = {'bnid_form': bform,
+                   'bnid': bnid_obj.bnid,
+                   'pk': bnid_obj.pk,
+                   'study_selector_form': ss_form}
+        context.update(csrf(request))
+        return render_to_response('viewer/bnid/edit_bnid.html', context,
+                                  context_instance=RequestContext(request))
+
+@user_passes_test(in_proj_user_group)
+def delete_bnid(request, bnid_id):
+    if request.method == 'POST':
+        Bnid.objects.get(pk=bnid_id).delete()
+        return HttpResponseRedirect('/viewer/bnid/')
+    else:
+        bnid_obj = Bnid.objects.get(pk=bnid_id)
+        context = {'bnid': bnid_obj.bnid, 'pk': bnid_obj.pk}
+        context.update(csrf(request))
+        return render(request, 'viewer/bnid/delete_bnid.html', context)
 
 '''
 Report model
 '''
+@user_passes_test(in_proj_user_group)
+def manage_report(request):
+    context = {'reports': Report.objects.all()}
+    context.update(csrf(request))
+    return render(request, 'viewer/report/manage_report.html', context)
+
 @user_passes_test(in_proj_user_group)
 def upload_report(request):
     if request.method == 'POST':
@@ -230,19 +269,16 @@ def upload_report(request):
             rform = ReportForm(request.POST)
         if rform.is_valid():
             rform.save()
-            return HttpResponseRedirect('/viewer/upload_report/')
+            return HttpResponseRedirect('/viewer/report/')
         else:
             print "rform is Invalid"
             print str(rform)
     else:
         rform = ReportForm(instance=Report(), initial={})
         context = {'report_form': rform}
-        reports = Report.objects.all()
-        context['reports'] = reports
         context.update(csrf(request))
         return render_to_response('viewer/report/upload_report.html', context,
                                   context_instance=RequestContext(request))
-
 
 @user_passes_test(in_proj_user_group)
 def view_report(request, file_id):
@@ -266,6 +302,39 @@ def view_report(request, file_id):
     return render_to_response('viewer/report/view_report.html', context,
                               context_instance=RequestContext(request))
 
+def delete_report(request, report_id):
+    if request.method == 'POST':
+        Report.objects.get(pk=report_id).delete()
+        return HttpResponseRedirect('/viewer/report/')
+    else:
+        report_obj = Report.objects.get(pk=report_id)
+        context = {'name': report_obj.report_file.name.strip('./'), 'pk': report_obj.pk}
+        return render(request, 'viewer/report/delete_report.html', context)
+
+'''
+Search functions
+'''
+@user_passes_test(in_proj_user_group)
+def search_reports(request):
+    variant_fields = Variant._meta.get_all_field_names()
+    context = {'variant_fields':variant_fields}
+    return render(request, 'viewer/search/search_reports.html', context)
+
+
+@user_passes_test(in_proj_user_group)
+@ajax
+def ajax_search_reports(request, search_col, search_term, search_type):
+    db_lookup = '%s__%s' % (search_col, search_type)
+    variants = Variant.objects.filter(**{db_lookup: search_term})
+    #print variants[0].report.study.description
+    # from django.core import serializers
+    # vars = serializers.serialize('json', variants)
+    return report_parser.json_from_ajax(variants)
+
+
+'''
+Util functions
+'''
 @user_passes_test(in_proj_user_group)
 def get_samples(request, study_id=None, **kwargs):
     sample_dict = {}
@@ -301,19 +370,4 @@ def load_variants(request, report_id=None):
     return HttpResponseRedirect('/viewer/upload_report/')
 
 
-@user_passes_test(in_proj_user_group)
-def search_reports(request):
-    variant_fields = Variant._meta.get_all_field_names()
-    context = {'variant_fields':variant_fields}
-    return render(request, 'viewer/search/search_reports.html', context)
 
-
-@user_passes_test(in_proj_user_group)
-@ajax
-def ajax_search_reports(request, search_col, search_term, search_type):
-    db_lookup = '%s__%s' % (search_col, search_type)
-    variants = Variant.objects.filter(**{db_lookup: search_term})
-    #print variants[0].report.study.description
-    # from django.core import serializers
-    # vars = serializers.serialize('json', variants)
-    return report_parser.json_from_ajax(variants)
