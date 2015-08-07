e52c19cce574b06b3046708aecf2643580176737
Dominic Fitzgerald <dominicfitzgerald11@gmail.com>
2015-08-07 14:08:36 -0500

    Added search feature, modal boxes, rearranged internal structure

--- urls.py
+++ urls.py
@@ -17,35 +17,57 @@ urlpatterns = patterns('',
                            name='permission'),
 
                        # Bnid
-                       url(r'^new_bionimbus_id/$',
-                           views.new_bionimbus_id, name='new_bionimbus_id'),
+                       url(r'^bnid/$',
+                           views.manage_bnid, name='manage_bnid'),
+                       url(r'^bnid/new_bnid/$',
+                           views.new_bnid, name='new_bnid'),
+                       url(r'bnid/edit_bnid/(?P<bnid_id>\d+)/$',
+                           views.edit_bnid, name='edit_bnid'),
+                       url(r'bnid/delete_bnid/(?P<bnid_id>\d+)/$',
+                           views.delete_bnid, name='delete_bnid'),
                        url(r'^get_bnids_by_study/(?P<study_id>\d+)/$',
                            views.get_bnids_by_study, name='get_bnids_by_study'),
-                       url(r'bnid_edit/(?P<bnid_id>\d+)/$',
-                           views.edit_bionimbus_id, name='bnid_edit'),
+
 
                        # Sample
-                       url(r'^new_sample/$', views.new_sample,
+                       url(r'^sample/$', views.manage_sample,
+                           name='manage_sample'),
+                       url(r'^sample/new_sample/$', views.new_sample,
                            name='new_sample'),
+                       url(r'edit_sample/(?P<sample_id>\d+)/$', views.edit_sample,
+                           name='edit_sample'),
+                       url(r'delete_sample/(?P<sample_id>\d+)/$', views.delete_sample,
+                           name='delete_sample'),
                        url(r'^get_samples/(?P<study_id>\d+)/$', views.get_samples,
                            name='get_samples'),
-                       url(r'sample_edit/(?P<sample_id>\d+)/$',
-                           views.edit_sample, name='sample_edit'),
 
                        # Study
-                       url(r'^new_study/$', views.new_study, name='new_study'),
-                       url(r'study_edit/(?P<study_id>\d+)/$',
-                           views.edit_study, name='study_edit'),
+                       url(r'^study/$', views.manage_study, name='manage_study'),
+                       url(r'^study/new_study/$', views.new_study, name='new_study'),
+                       url(r'^study/edit_study/(?P<study_id>\d+)/$',
+                           views.edit_study, name='edit_study'),
+                       url(r'^study/delete_study/(?P<study_id>\d+)/$',
+                           views.delete_study, name='delete_study'),
 
                        # Report
-                       url(r'^upload_report/$', views.upload_report,
+                       url(r'^report/$', views.manage_report,
+                           name='manage_report'),
+                       url(r'report/view_report/(?P<file_id>\d+)/$',
+                           views.view_report, name='view_report'),
+                       url(r'^report/upload_report/$', views.upload_report,
                            name='upload_report'),
+                       url(r'^report/delete_report/(?P<report_id>\d+)/$', views.delete_report,
+                           name='delete_report'),
                        url(r'^load_variants/(?P<report_id>\d+)/$', views.load_variants,
                            name='load_variants'),
+
                        url(r'^files/(?P<path>.*)$',
                            'django.views.static.serve',
                            {'document_root': settings.MEDIA_ROOT}),
-                       url(r'view_report/(?P<file_id>\d+)/$',
-                           views.view_report, name='view_report'),
+
+                       # Search
+                       url(r'^search/$', views.search_reports, name='search_reports'),
+                       url(r'^ajax_search_reports/(?P<search_col>\S+)/(?P<search_term>\S+)/(?P<search_type>\S+)/$',
+                           views.ajax_search_reports, name='ajax_search_reports'),
 
                        ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- util/report_parser.py
+++ util/report_parser.py
@@ -12,14 +12,16 @@ def add_goodies(atoms, headers, md_anderson_genes, eMERGE_genelist):
             atoms[i] = ''
         # highlight NON_SYNONYMOUS_CODING
         if headers[i] == 'effect' and atoms[i] == 'NON_SYNONYMOUS_CODING':
-            atoms[i] = "<font color=green>%s</font>" % atoms[i]
+            atoms[i] = "<span style=\"color:green\">%s</span>" % atoms[i]
         # add MDAnderson and eMERGE links to appropriate gene names
         if (headers[i] == 'gene') and (atoms[i] is not None):
             # link to genecards
-            gene_card = '<a href=\"http://www.genecards.org/index.php?path=/Search/keyword/{g}\" target=\"_blank\" title=\"GeneCards Link\">{g}'.format(g=atoms[i])
+            gene_card = ('<a href=\"http://www.genecards.org/index.php?' +
+                         'path=/Search/keyword/{g}\" target=\"_blank\" ' +
+                         'title=\"GeneCards Link\">{g}</a>').format(g=atoms[i])
 
             # construct additional links out
-            gene_str_extension = '<br><font size=-2>'
+            gene_str_extension = '<br/><span style=\"font-size:x-small\">'
 
             if atoms[i].lower() in md_anderson_genes:
                 gene_str_extension += '<a href=\"{link}\" target=\"_blank\" title=\"MDAnderson Cancer Center\">MDAnderson</a>'.format(
@@ -29,13 +31,25 @@ def add_goodies(atoms, headers, md_anderson_genes, eMERGE_genelist):
                 gene_str_extension += ' eMERGE'
 
             # assemble additional links
-            new_link = '{gene_card}{ext}</font>'.format(gene_card=gene_card,
+            new_link = '{gene_card}{ext}</span>'.format(gene_card=gene_card,
                                                    ext=gene_str_extension)
             # apply changes
             atoms[i] = new_link
 
     return atoms
 
+def get_mdanderson_genes():
+    with open(settings.LINKS_OUT + 'mdanderson.json', 'r') as md_f:
+        return json.loads(json.load(md_f))
+
+def get_eMERGE_genelist():
+    eMERGE_genelist = []
+    with open(settings.LINKS_OUT + 'eMERGE_genelist.txt', 'r') as e_f:
+        for line in e_f:
+            gene = line.rstrip()
+            if gene:
+                eMERGE_genelist.append(gene)
+    return eMERGE_genelist
 
 def json_from_report(filename):
     print "%s - creating json from: %s" % (os.getcwd(), filename)
@@ -46,15 +60,8 @@ def json_from_report(filename):
         splitby = '\t'
     cols = header_line.split(splitby)
 
-    with open(settings.LINKS_OUT + 'mdanderson.json', 'r') as md_f:
-        md_anderson_genes = json.loads(json.load(md_f))
-
-    eMERGE_genelist = []
-    with open(settings.LINKS_OUT + 'eMERGE_genelist.txt', 'r') as e_f:
-        for line in e_f:
-            gene = line.rstrip()
-            if gene:
-                eMERGE_genelist.append(gene)
+    md_anderson_genes = get_mdanderson_genes()
+    eMERGE_genelist = get_eMERGE_genelist()
 
     d = []
     for line in report_file:
@@ -67,6 +74,91 @@ def json_from_report(filename):
                 d.append(formatted_line)
     data = tablib.Dataset(*d, headers=cols)
     return data
+
+def json_from_ajax(db_response):
+    # Make sure query returned some data
+    # If not, report back empty
+    if len(db_response) == 0:
+        return ''
+
+    md_anderson_genes = get_mdanderson_genes()
+    eMERGE_genelist = get_eMERGE_genelist()
+
+    # Pull headers, sort into alphabetical order
+    # This is so it's in the same order returned
+    # by Variant._meta.get_all_field_names()
+    headers = []
+    for header in db_response.values()[0]:
+        headers.append(header)
+    headers.sort()
+
+    # Grab and store pre-rearranged indexes of relevant headers
+    col_order = [headers.index('chrom'), headers.index('pos'), headers.index('gene_name'), headers.index('ref'),
+                  headers.index('alt'), headers.index('normal_ref_count'), headers.index('normal_alt_count'),
+                  headers.index('tumor_ref_count'), headers.index('tumor_alt_count')]
+
+    # Rearrage headers
+    new_headers = []
+    for i in col_order:
+        new_headers.append(headers[i])
+        headers[i] = None
+    map(lambda x: new_headers.append(x) if x else False, headers)
+    headers = new_headers
+
+    # Append extra headers to front
+    headers.insert(0, 'BnIDs')
+    headers.insert(0, 'sample')
+    headers.insert(0, 'study')
+
+    # Temporary to make search and view report agree on the header for the gene name
+    # TODO
+    headers[headers.index('gene_name')] = 'gene'
+
+    # Parse list of variant records
+    variant_records = []
+    for variant in db_response:
+        # Get all field names, delete report object
+        field_names = Variant._meta.get_all_field_names()
+        del field_names[field_names.index('report')]
+
+        # Pull relevent fields from variant object
+        record = []
+        for key in field_names:
+            if key in variant.__dict__:
+                record.append(variant.__dict__[key])
+
+        # Replace all None with empty string
+        record = ['' if elem is None else elem for elem in record]
+
+        # Sort the records into desired order
+        new_record = []
+        for i in col_order:
+            new_record.append(record[i])
+            record[i] = None
+        map(lambda x: new_record.append(x) if x is not None else False, record)
+        record = new_record
+
+        # Append extra data to front
+        bnids, samples = [], set()
+        for bnid in variant.report.bnids.all():
+            bnids.append(bnid.bnid)
+            samples.add(bnid.sample.name)
+        bnids = map(str, bnids)
+        samples = map(str, samples)
+        study = variant.report.study.name
+        record = [study, str(samples), str(bnids)] + record
+
+        # This doesn't allow through records that have INTRON or INTERGENIC
+        # Is that what we want?
+        # TODO
+        if len(record) > 1:
+            if 'INTRON' not in record and 'INTERGENIC' not in record:
+                formatted_record = add_goodies(record, headers, md_anderson_genes, eMERGE_genelist)
+                # Append this record to list of all records
+                variant_records.append(formatted_record)
+
+    # Return as an html table
+    return tablib.Dataset(*variant_records, headers=headers).html
 
 
 def load_into_db(report):
